9cf86c045852d753d08bbd4d4c75ba706e345216
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2022-01-15 20:58:38 -0300

    add comentario

--- src/controllers/Crypto.js
+++ src/controllers/Crypto.js
@@ -1,3 +1,12 @@
+/**
+ * Esto no se usa
+ * Se va a implementar del lado del cliente
+ * En la app movil con ionic/angular
+ */
+
+
+
+
 const Crypto = require("crypto-js")
 
 module.exports = class CryptoController {
