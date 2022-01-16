54403b75f1f631e12d748f9308873c77740b87ce
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2022-01-15 22:10:24 -0300

    UPDATE getAllUsers -> password no va al cliente

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,11 +1,20 @@
 const Database = require("./Database")
+const Query = require("./Query")
 
 
 
 module.exports = class UsersController {
 
-    static getAllUsers() {
+    static getAllUsers(req, res) {
 
+        new Query("SELECT U.id_user, u.username FROM ALL_USERS").select()
+            .then(data => {
+                res.status(200).json(JSON.parse(data))
+            })
+            .catch(error => {
+                console.log(error)
+                res.status(500).json({ error: true, message: JSON.stringify(error) })
+            })
     }
 
     static new() {
