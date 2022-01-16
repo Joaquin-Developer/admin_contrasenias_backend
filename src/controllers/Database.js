b8c562bd2a0a5516d4f8509f9fbf3388634c65c9
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2022-01-15 21:56:26 -0300

    UPDATE getConnection -> formas de obtener autenticacion segun modo

--- src/controllers/Database.js
+++ src/controllers/Database.js
@@ -5,11 +5,28 @@ const config = require("../environments/environment")
 class Database {
 
     static getConnection() {
+        if (config["IN_PRODUCTION"]) {
+            if (config["PRODUCTION_DATABASE"]) {
+                return mysql.createConnection({
+                    host: config.PRODUCTION_DATABASE.HOST,
+                    user: config.PRODUCTION_DATABASE.USER,
+                    password: config.PRODUCTION_DATABASE.PASSWORD,
+                    database: config.PRODUCTION_DATABASE.NAME
+                })
+            }
+            return mysql.createConnection({
+                host: process.env.HOST,
+                user: process.env.USER,
+                password: process.env.PASSWORD,
+                database: process.env.NAME
+            })
+        }
+
         return mysql.createConnection({
-            host: (config['IN_PRODUCTION']) ? process.env.HOST : config.DEVELOPMENT_DATABASE.HOST,
-            user: (config['IN_PRODUCTION']) ? process.env.USER : config.DEVELOPMENT_DATABASE.USER,
-            password: (config['IN_PRODUCTION']) ? process.env.PASSWORD : config.DEVELOPMENT_DATABASE.PASSWORD,
-            database: (config['IN_PRODUCTION']) ? process.env.NAME : config.DEVELOPMENT_DATABASE.NAME
+            host: config.DEVELOPMENT_DATABASE.HOST,
+            user: config.DEVELOPMENT_DATABASE.USER,
+            password: config.DEVELOPMENT_DATABASE.PASSWORD,
+            database: config.DEVELOPMENT_DATABASE.NAME
         })
     }
 
