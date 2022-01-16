5f4e27ebdaef75e18b2a73c131061a235a9b1054
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2022-01-15 22:20:50 -0300

    UPDATE -> Agrego query y manejo de promesa

--- src/controllers/records.controller.js
+++ src/controllers/records.controller.js
@@ -1,3 +1,4 @@
+const Query = require("./Query")
 
 class Records {
 
@@ -14,8 +15,39 @@ class Records {
      * no obtiene la contraseña en sí, solo los registros 
      * de contraseñas existentes en la BD
      */
-    static getAllPasswordsRecordsByUser() {
+    static getAllPasswordsRecordsByUser(req, res) {
+        let { idUser, username } = req.query
+        username = String(username)
 
+        if (idUser) {
+            idUser = parseInt(idUser)
+        } else {
+            idUser = -1
+        }
+
+        const QUERY = `
+            SELECT 
+                au.username,
+                pr.site_name,
+                pr.site_username,
+                pr.site_mail,
+                pr.id_passw_encrypted
+            FROM 
+                PASSW_RECORDS pr
+                JOIN ALL_USERS au ON pr.id_user = au.id_user
+            WHERE
+                au.username = '${username}' OR au.id_user = ${idUser}
+        `
+
+        new Query(QUERY).select()
+            .then(data => {
+                // console.log(data)
+                res.status(200).json(JSON.parse(data))
+            })
+            .catch(error => {
+                console.log(error)
+                res.status(500).json({ error: true, message: "Internal server error" })
+            })
     }
 
     static new() {
